a772735136bf1bebe4661363662a0db06f8e1827
FADEL SULTAN <srqa3h.com@gmail.com>
2025-12-17 12:58:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 # التوكن من متغيرات البيئة
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("8370038083:AAEpJzgriunEy-mnPi7efbPeEy6KSo5H9LA")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     welcome = """
